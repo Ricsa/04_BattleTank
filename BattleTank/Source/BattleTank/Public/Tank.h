71051f7ca31a94a8427b211879f308196444acbb
Deme András <doroty666evil@hotmail.com>
2017-11-30 18:54:24 +0100

    Added Aiming component for the tank with C++ in runtime (protected from designers)

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -14,6 +15,10 @@ class BATTLETANK_API ATank : public APawn
 public:
 
 	void AimAt(FVector OutHitLocation);
+
+protected:
+
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 
