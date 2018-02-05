cf1b3f89728a0ab653a836d901bc381fea802782
Deme András <doroty666evil@hotmail.com>
2018-02-05 03:32:05 +0100

    Barrel can now move (pitch)

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -6,8 +6,11 @@
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	// Move the barrel to the right amount this frame
-
 	// Given a max elevation speed and the frame time
-
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevation, MaxElevation);
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
