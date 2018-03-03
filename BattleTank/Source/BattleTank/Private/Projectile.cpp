06144f1ad66313735a31546f313bdc4592e5d4a4
Deme András <doroty666evil@hotmail.com>
2018-03-03 07:30:15 +0100

    Added Health Bar.

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -6,6 +6,7 @@
 #include "PhysicsEngine/RadialForceComponent.h"
 #include "Engine/World.h"
 #include "TimerManager.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AProjectile::AProjectile()
@@ -54,6 +55,15 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, U
 
 	SetRootComponent(ImpactBlast);
 	CollisionMesh->DestroyComponent();
+
+	UGameplayStatics::ApplyRadialDamage(
+										this, 
+										ProjectileDamage, 
+										GetActorLocation(), 
+										ExplosionForce->Radius, // for consistency
+										UDamageType::StaticClass(),
+										TArray<AActor*>()		// damage all actors
+										);
 
 	FTimerHandle Timer;
 	GetWorld()->GetTimerManager().SetTimer(Timer, this, &AProjectile::OnTimerExpire, DestroyDelay, false);
